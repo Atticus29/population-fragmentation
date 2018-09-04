8ab9bab1add515df8e8b7c335cf5bf15a73263c4
Mark <mark.aaron.fisher@gmail.com>
2018-09-04 15:26:27 -0700

    begin implementing lizard draw. Dry up drawingService code

--- src/app/drawing.service.ts
+++ src/app/drawing.service.ts
@@ -28,39 +28,28 @@ export class DrawingService {
     let canvasElement = <HTMLCanvasElement>document.getElementById(elementId);
     if (canvasElement){
       let context = canvasElement.getContext("2d");
-      // the triangle
       context.beginPath();
       context.moveTo(x1, y1);
       context.lineTo(x2, y2);
       context.lineTo(x3, y3);
       context.closePath();
-
-      // the outline
       context.lineWidth = 1;
       context.strokeStyle = color;
       context.stroke();
-
-      // the fill color
       if(fillStatus){
       context.fillStyle = color;
       context.fill();
     }
-
   }
 }
 
 drawEllipse(x: number, y: number, radiusX: number, radiusY: number, rotation: number, startAngle:number, endAngle:number, elementId: string, color: string, fillStatus: boolean){
   let canvas = <HTMLCanvasElement> document.getElementById(elementId);
   let ctx = canvas.getContext('2d');
-
   ctx.setLineDash([])
   ctx.beginPath();
   ctx.ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle); //x, y, radiusX, radiusY, rotation, startAngle, endAngle, anticlockwise
   ctx.stroke();
-  // ctx.setLineDash([5,5])
-  // ctx.moveTo(0, 200);
-  // ctx.lineTo(200,0);
-  // ctx.stroke();
   if(fillStatus){
     ctx.fillStyle = color;
     ctx.fill();
--- src/app/lizard-display/lizard-display.component.ts
+++ src/app/lizard-display/lizard-display.component.ts
@@ -12,7 +12,7 @@ export class LizardDisplayComponent implements OnInit {
   constructor(private ds: DrawingService) { }
 
   ngOnInit() {
-    this.ds.drawTriangle(100, 50, 100, 150, 0, 100, 'lizard-canvas',"black", true);
+    this.ds.drawTriangle(37.5,87.5,37.5,112.5,25,100, 'lizard-canvas',"black", true);
     // this.ds.drawArc(100, 100, 50, 0, Math.PI, 'lizard-canvas',"black", true);
     // this.ds.drawEllipse(100, 100, 50, 25, 45 * Math.PI/180,  0, 2 * Math.PI, 'lizard-canvas', 'black', false);
     // this.ds.drawEllipse(100, 100, 50, 25, 0, 0, 2*Math.PI, 'lizard-canvas', 'black', true);
