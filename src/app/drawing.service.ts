4d6c0b02ef1822b820b63debee77d61e128edf9f
Mark <mark.aaron.fisher@gmail.com>
2018-09-04 16:22:42 -0700

    add eyes and make polka dots random colors

--- src/app/drawing.service.ts
+++ src/app/drawing.service.ts
@@ -57,4 +57,33 @@ drawEllipse(x: number, y: number, radiusX: number, radiusY: number, rotation: nu
   }
   ctx.closePath();
 }
+
+drawLizard(canvasId: string){
+  //head
+  this.drawTriangle(37.5,87.5,37.5,112.5,25,100, canvasId,"black", true);
+  this.drawArc(37.5, 100, 12.5, 3*Math.PI/2, Math.PI/2, canvasId,"black", true);
+
+  //eyes
+  this.drawArc(37.5, 106.25, 2, 0, 2*Math.PI, canvasId,"white", true);
+  this.drawArc(37.5, 93.75, 2, 0, 2*Math.PI, canvasId,"white", true);
+  this.drawArc(37.5, 106.25, 0.5, 0, 2*Math.PI, canvasId,"black", true);
+  this.drawArc(37.5, 93.75, 0.5, 0, 2*Math.PI, canvasId,"black", true);
+
+  //body
+  this.drawEllipse(100, 100, 50, 10, 0, 0, 2 * Math.PI, canvasId, 'black', true);
+
+  //legs
+  this.drawEllipse(140, 110, 30, 3, 0.65, 0, 2 * Math.PI, canvasId, 'black', true);
+  this.drawEllipse(140, 90, 30, 3, 0.9+Math.PI/2, 0, 2 * Math.PI, canvasId, 'black', true);
+  this.drawEllipse(70, 110, 30, 3, -Math.PI/6, 0, 2 * Math.PI, canvasId, 'black', true);
+  this.drawEllipse(70, 90, 30, 3, Math.PI/6, 0, 2 * Math.PI, canvasId, 'black', true);
+
+  //random polka dots
+  let colorArray = new Array<string>("blue", "pink", "orange", "#FF00FF", "red", "#00FFFF", "#800000", "#00FF00", "#008000", "#00FFFF", "#008080", "#BFBFFE", "#800080");
+  this.drawArc(60, 101, 3, 0, 2*Math.PI, canvasId, colorArray[Math.floor(Math.random()*colorArray.length)], true);
+  this.drawArc(77, 95, 3, 0, 2*Math.PI, canvasId, colorArray[Math.floor(Math.random()*colorArray.length)], true);
+  this.drawArc(100, 102, 3, 0, 2*Math.PI, canvasId, colorArray[Math.floor(Math.random()*colorArray.length)], true);
+  this.drawArc(109, 94, 3, 0, 2*Math.PI, canvasId, colorArray[Math.floor(Math.random()*colorArray.length)], true);
+  this.drawArc(120, 102, 3, 0, 2*Math.PI, canvasId, colorArray[Math.floor(Math.random()*colorArray.length)], true);
+}
 }
--- src/app/lizard-display/lizard-display.component.ts
+++ src/app/lizard-display/lizard-display.component.ts
@@ -12,25 +12,9 @@ export class LizardDisplayComponent implements OnInit {
   constructor(private ds: DrawingService) { }
 
   ngOnInit() {
-    //head
-    this.ds.drawTriangle(37.5,87.5,37.5,112.5,25,100, 'lizard-canvas',"black", true);
-    this.ds.drawArc(37.5, 100, 12.5, 3*Math.PI/2, Math.PI/2, 'lizard-canvas',"black", true);
-
-    //body
-    this.ds.drawEllipse(100, 100, 50, 10, 0, 0, 2 * Math.PI, 'lizard-canvas', 'black', true);
-
-    //legs
-    this.ds.drawEllipse(140, 110, 30, 3, 0.65, 0, 2 * Math.PI, 'lizard-canvas', 'black', true);
-    this.ds.drawEllipse(140, 90, 30, 3, 0.9+Math.PI/2, 0, 2 * Math.PI, 'lizard-canvas', 'black', true);
-    this.ds.drawEllipse(70, 110, 30, 3, -Math.PI/6, 0, 2 * Math.PI, 'lizard-canvas', 'black', true);
-    this.ds.drawEllipse(70, 90, 30, 3, Math.PI/6, 0, 2 * Math.PI, 'lizard-canvas', 'black', true);
-
-    //polka dots
-    this.ds.drawArc(60, 101, 3, 0, 2*Math.PI, 'lizard-canvas',"blue", true);
-    this.ds.drawArc(77, 95, 3, 0, 2*Math.PI, 'lizard-canvas',"pink", true);
-    this.ds.drawArc(100, 102, 3, 0, 2*Math.PI, 'lizard-canvas',"orange", true);
-    this.ds.drawArc(109, 94, 3, 0, 2*Math.PI, 'lizard-canvas',"white", true);
-    this.ds.drawArc(120, 102, 3, 0, 2*Math.PI, 'lizard-canvas',"green", true);
+      this.ds.drawLizard('lizard-canvas1');
+      this.ds.drawLizard('lizard-canvas2');
+      this.ds.drawLizard('lizard-canvas3');
   }
 
 }
