e37c11a2ed1f9e37af60dc9fba19c71e4e1ce8c5
Mark <mark.aaron.fisher@gmail.com>
2018-11-12 11:55:53 -0800

    add previous buttons to some of the components

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./introduction.component.css']
 })
 export class IntroductionComponent implements OnInit {
+  private heading: string = "Introduction";
   private text: string = "In this simulation, you will be learning about how population fragmentation increases the rate at which genetic diversity is lost. The class will be simulating two populations—one intact, and one fragmented into three subpopulations that can’t reach (and therefore can’t mate with) each other. You’ll be simulating a population of snarl-toothed draggles. Scientists have recently discovered that a single gene fully controls spot color, which is the gene you’ll be tracking through the generations."
 
   constructor() { }
