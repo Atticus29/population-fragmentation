b533f1ced26f324475a7bc7ac0c2554178f7b2c5
Mark <mark.aaron.fisher@gmail.com>
2019-06-28 13:14:16 -0700

    working through tutorial here https://medium.com/appgambit/part-1-serverless-web-app-using-aws-amplify-and-angular-c04caf09d83f and get a lot of stuff configured. Currently a successful build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,22 @@ import { Subject } from "rxjs";
 
 import {PopulationManagerService } from './population-manager.service';
 
+import Amplify from 'aws-amplify';
+import aws_exports from '../aws-exports';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  message = '';
 
   constructor(){}
 
   ngOnInit() {
+    Amplify.configure(aws_exports);
+    console.log('Amplify Initialised');
+    this.message = "Amplify Initialised";
   }
 }
