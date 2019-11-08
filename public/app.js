fc35cd490a10f28559bbf46af023841d9cf1c79a
Mark <mark.aaron.fisher@gmail.com>
2019-11-08 15:50:14 -0800

    up to deploying attachSource function successfully working

--- public/app.js
+++ public/app.js
@@ -42,4 +42,50 @@ document.addEventListener('DOMContentLoaded', function(){
 
     console.log(response);
   };
+
+  var stripe = Stripe('pk_test_NKyjLSwnMosdX0mIgQaRRHbS');
+  var elements = stripe.elements();
+  const style = {
+    base: {
+      fontSize: '16px',
+      color: "#32325d",
+    },
+  };
+
+  // Create an instance of the card Element.
+  const card = elements.create('card', {style});
+
+  // Add an instance of the card Element into the 'card-element' div
+  card.mount('#card-element');
+
+  card.addEventListener('change', function(event){
+    var displayError = document.getElementById('card-errors');
+    if(event.error){
+      displayError.textContent = event.error.message;
+    } else{
+      displayError.textContent = '';
+    }
+  });
+
+  const form = document.getElementById('payment-form');
+  form.addEventListener('submit', async(event) =>{
+    event.preventDefault();
+    const {source, error} = await stripe.createSource(card);
+    console.log(source);
+    if(error){
+      const errorElement = document.getElementById('card-errors');
+      errorElement.textContent = error.message;
+    }else{
+      // Send the token to server
+      sourceHandler(source);
+    }
+  });
+
+  const attachFun = fun.httpsCallable('stripeAttachSource');
+  const sourceHandler = async(source) => {
+    console.log(source.id);
+    const res = await attachFun({source: source.id});
+    console.log(res);
+    alert("Success! Source attached to customer!");
+  }
 });
