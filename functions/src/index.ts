6ca17fff33b0d993bcf1ca10c4c7725bfe342f58
Mark <mark.aaron.fisher@gmail.com>
2019-11-07 16:44:17 -0800

    begin following stripe setup tutorial

---
+++ functions/src/index.ts
@@ -0,0 +1,10 @@
+import * as functions from 'firebase-functions';
+
+export const testFunction = functions.https.onCall( async (data, context) => {
+    const uid  = context.auth && context.auth.uid;
+    const message = data.message;
+
+    return `${uid} sent a message of ${message}`
+});
+
+export { stripeAttachSource } from './sources';
